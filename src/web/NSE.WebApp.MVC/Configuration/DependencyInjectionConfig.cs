0d3e8faa7bba4acc8d86dbea3ab1f442bbc087a1
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2020-05-11 18:39:14 -0300

    obtendo dados do usuario do cookie

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 
 namespace NSE.WebApp.MVC.Configuration
@@ -11,6 +13,10 @@ namespace NSE.WebApp.MVC.Configuration
         {
             // registro da abstração do http client
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddScoped<IUser, AspNetUser>();
         }
     }
 }
