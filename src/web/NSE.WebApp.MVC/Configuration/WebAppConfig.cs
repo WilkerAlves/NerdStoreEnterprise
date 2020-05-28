a11c73c5c0fd50ce7cc920f1b9aca1b7b564ab4e
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2020-05-28 15:03:22 -0300

    melhorias da webapp

--- src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -39,6 +41,15 @@ namespace NSE.WebApp.MVC.Configuration
             app.UseRouting();
 
             app.UseIdentityConfiguration();
+
+            var supportedCultures = new[] {new CultureInfo("pt-BR"),};
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("pt-BR"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+
 
             //registro do middleware customizado
             app.UseMiddleware<ExceptionMiddleware>();
