1efa3fab587548ba82956d04925cee62150ba262
WilkerAlves <wilkeralves97@gmail.com>
2020-12-15 11:48:10 -0300

    ponto de partida para api de carrinho

---
+++ src/services/NSE.Carrinho.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace NSE.Carrinho.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
